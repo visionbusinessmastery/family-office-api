8f63ac46c290f608f521ab04c4f4faa1b53d6d48
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-03-12 00:21:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -110,42 +110,6 @@ class UserRegister(BaseModel):
     email: str
     password: str
 
-# ==================================================
-# AUTH SYSTEM
-# ==================================================
-
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-def create_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
-    to_encode.update({"exp": expire})
-
-    return jwt.encode(
-        to_encode,
-        SECRET_KEY,
-        algorithm=ALGORITHM
-    )
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email = payload.get("sub")
-
-        if email is None:
-            raise HTTPException(status_code=401, detail="Token invalide")
-
-        return email
-
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Token invalide")
-
 # ======================
 # REGISTER
 # ======================
@@ -196,6 +160,44 @@ def register(user: UserRegister):
             detail=str(e)
         )
 
+# ==================================================
+# AUTH SYSTEM
+# ==================================================
+
+def hash_password(password: str) -> str:
+    password = password[:72]
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    plain_password = plain_password[:72]
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(status_code=401, detail="Token invalide")
+
+        return email
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token invalide")
+        
 # ==================================================
 # DATABASE
 # ==================================================
@@ -805,6 +807,7 @@ def schema():
             WHERE table_name='users'
         """))
         return [row[0] for row in result]
+
 
 
 
