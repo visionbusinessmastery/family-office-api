c37f32e344c19cdec670f8da591e72c65f62d471
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-03-30 09:15:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -394,7 +394,7 @@ def get_stock_data(ticker: str):
             "ticker": ticker,
             "price": None,
             "error": str(e)
-        }
+}
 
 # ==================================================
 # STOCK ROUTE
