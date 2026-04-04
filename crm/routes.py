3af3a86fd332bc43b5146bf6014b9e4ada34cad1
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 13:10:12 -0400

    Update routes.py

--- crm/routes.py
+++ crm/routes.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
 from crm.odoo import OdooClient
 
+# ==================================================
+# CONFIG ODOO
+# ==================================================
+
 router = APIRouter()
 odoo = OdooClient()
 
+# ==================================================
+# CREATE LEAD ODOO
+# ==================================================
 @router.post("/lead")
 def create_lead(name: str, email: str):
     contact_id = odoo.create_contact(name, email)
