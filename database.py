c8bf65e7f4376670a3fcfe7e466a75a176aa4092
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 09:00:53 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,34 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+# 1. Récupérer la vraie DB Render
 DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
+# Sécurité (fallback local)
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./test.db"
+
+# 2. Créer engine APRÈS
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# 3. Session
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
+# 4. Base
 Base = declarative_base()
 
+# 5. Dependency FastAPI
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+
