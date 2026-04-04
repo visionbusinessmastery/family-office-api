b76ed45c0a699260fc00f118e3dc13e3b7d7fd0c
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 13:19:34 -0400

    Update routes.py

--- portfolio/routes.py
+++ portfolio/routes.py
@@ -2,8 +2,15 @@ from fastapi import APIRouter, Depends
 from auth.routes import get_current_user
 from portfolio.service import get_user_portfolio
 
+# ==================================================
+# CONFIG PROTFOLIO
+# ==================================================
+
 router = APIRouter()
 
+# ==================================================
+# GET PORTFOLIO
+# ==================================================
 @router.get("/")
 def get_portfolio(user: str = Depends(get_current_user)):
     data = get_user_portfolio(user)
