006ab762323db3ea12d5a54c6039c5ec45c96950
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 11:33:16 -0400

    Create routes.py

---
+++ portfolio/routes.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Depends
+from auth.routes import get_current_user
+from portfolio.service import get_user_portfolio
+
+router = APIRouter()
+
+@router.get("/")
+def get_portfolio(user: str = Depends(get_current_user)):
+    data = get_user_portfolio(user)
+    return {"portfolio": [dict(r._mapping) for r in data]}
