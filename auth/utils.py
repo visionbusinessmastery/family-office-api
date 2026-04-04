d4e3521ff120caa1351203f27da82accb9b3310b
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 16:23:50 -0400

    Update utils.py

--- auth/utils.py
+++ auth/utils.py
@@ -14,7 +14,6 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/
 
 # =========================
 # PASSWORD
