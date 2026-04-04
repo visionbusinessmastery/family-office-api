1eec658cdb8bf145af20503bb03f9577ea240591
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 18:43:45 -0400

    Update schemas.py

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field, EmailStr
-from typing import Optional, Optional
+from typing import Optional, Optional, str
 
 # ==================================================
 # MODELS
