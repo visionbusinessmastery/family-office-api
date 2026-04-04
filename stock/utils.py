4b368f14066d962fa1d4780ab14ed0e37d7bc389
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 13:47:43 -0400

    Create utils.py

---
+++ stock/utils.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, EmailStr, Field
+import time
+import yfinance as yf
+import models
+
+# ==================================================
+# CONFIG STOCK
+# ==================================================
+
+ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+FMP_API_KEY = os.getenv("FMP_API_KEY")
+
+# ==================================================
+# MODELS
+# ==================================================
+
+class StockRequest(BaseModel):
+    ticker: str
+
+class Asset(BaseModel):
+    asset: str
+    asset_type: str
+    quantity: float
+    buy_price: float
+
