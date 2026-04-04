acdc6ed53222de5d97c01713ee47ffb522b2bb40
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 12:50:11 -0400

    Update utils.py

--- auth/utils.py
+++ auth/utils.py
@@ -8,6 +8,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def hash_password(password: str):
     return pwd_context.hash(password)
@@ -20,6 +21,13 @@ def create_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("sub")
+    except:
+        raise HTTPException(status_code=401, detail="Token invalide")
 
  if email is None:
             raise HTTPException(status_code=401, detail="Token invalide")
