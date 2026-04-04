ff4d58317db768bd92f255c87ee6971d77dd19e8
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 19:54:27 -0400

    Update routes.py

--- portfolio/routes.py
+++ portfolio/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from auth.routes import get_current_user
 from portfolio.service import get_user_portfolio
-from auth.schemas import StockRequest
+from .schemas import StockRequest
 import os
 
 # ==================================================
