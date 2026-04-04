aab304a0da6e9b54a551d159bd161472dc6e0dd9
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-04 16:16:03 -0400

    Update utils.py

--- auth/utils.py
+++ auth/utils.py
@@ -1,10 +1,12 @@
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 import os
 
 # ==================================================
-# CONFIG AUTH PASWWORD
+# CONFIG AUTH PASSWORD
 # ==================================================
 
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -12,38 +14,33 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/
 
-# ==================================================
-# HASH PASSWORD
-# ==================================================
+# =========================
+# PASSWORD
+# =========================
 def hash_password(password: str):
     return pwd_context.hash(password)
 
-# ==================================================
-# VRIFY PASSWORD
-# ==================================================
+
 def verify_password(password: str, hashed: str):
     return pwd_context.verify(password, hashed)
 
-# ==================================================
-# CREATE TOKEN
-# ==================================================
+# =========================
+# TOKEN
+# =========================
 def create_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
+
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-# ==================================================
-# GET CURRENT USER
-# ==================================================
-def get_current_user(token: str = Depends(oauth2_scheme)):
+
+def decode_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload.get("sub")
-    except:
-        raise HTTPException(status_code=401, detail="Token invalide")
+        email = payload.get("sub")
 
         if email is None:
             raise HTTPException(status_code=401, detail="Token invalide")
