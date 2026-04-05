7eebdca2b0ed0387cee8920c0b6ddc8128e43369
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-05 18:59:08 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-class User(BaseModel):
+class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
-class Portfolio(BaseModel):
+class Portfolio(Base):
     __tablename__ = "portfolio"
 
     id = Column(Integer, primary_key=True)
