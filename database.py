0da1b0031aba88fe19f7606e5a3cd180306be6ce
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-05 09:31:29 -0400

    Refactor database.py for SessionLocal and Base
    Updated SessionLocal configuration and adjusted Base declaration.

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Float
-from database import Base
 import os
 
-Base = declarative_base()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
