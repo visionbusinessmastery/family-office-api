b8df26c93ea935361884d31ea11305ce4f0a16ae
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-05 19:54:36 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
@@ -24,5 +23,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
