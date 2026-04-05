af28ac960eebd9f40275382ccf446122c398657f
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-05 14:16:53 -0400

    Update models.py

--- models.py
+++ models.py
@@ -7,3 +7,12 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+
+class Portfolio(BaseModel):
+    __tablename__ = "portfolio"
+
+    id = Column(Integer, primary_key=True)
+    asset = Column(String)
+    asset_type = Column(String)
+    quantity = Column(Integer)
+    buy_price = Column(Float)
