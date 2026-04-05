6c11e9029c5d9516b87f5b76836f5ae195c4a634
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-05 18:25:27 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-class User(Base):
+class User(BaseModel):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
