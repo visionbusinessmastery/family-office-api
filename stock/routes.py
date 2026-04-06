250dea85ececc225586a14724becf845356527bc
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 10:46:20 -0400

    Update routes.py

--- stock/routes.py
+++ stock/routes.py
@@ -5,7 +5,7 @@ from .service import get_stock_data
 
 router = APIRouter()
 
-@router.post("/stocks/search")
+@router.post("/stock/search")
 def search_stock(request: StockRequest, user: str = Depends(get_current_user)):
 
     data = get_stock_data(request.query)
