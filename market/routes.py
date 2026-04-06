df24db9545349ca1fbcf5924ede154c39209ad0f
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 15:16:02 -0400

    Update routes.py

--- market/routes.py
+++ market/routes.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter, Depends
 from auth.utils import get_current_user
 from .schemas import MarketRequest
-from .service import get_market_intelligence
+from .service import get_market
 
 router = APIRouter()
 
-@router.post("/intelligence")
-def market_intelligence(request: MarketRequest, user: str = Depends(get_current_user)):
+@router.post("/market")
+def market(request: MarketRequest, user: str = Depends(get_current_user)):
     
-    data = get_market_intelligence(request.query)
+    data = get_market(request.query)
 
     return {
         "user": user,
-        "market_intelligence": data
+        "market": data
     }
