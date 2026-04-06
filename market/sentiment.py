2bc6cbb9cdcafd14d6dedd5452d82b298a5d1ab4
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 17:19:21 -0400

    Update sentiment.py

--- market/sentiment.py
+++ market/sentiment.py
@@ -1,9 +1,5 @@
 import requests
-from config import FMP_API_KEY
 from openai import OpenAI
-
-FMP_API_KEY = os.getenv("FMP_API_KEY")
-ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
