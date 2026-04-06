81d0dc64538c75aebb7946742cb442678a6c52c4
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 19:40:43 -0400

    Create agorastore.py

---
+++ real estate/scrappers/agorastore.py
@@ -0,0 +1,10 @@
+import requests
+
+def search_agorastore(city: str):
+
+    return [{
+        "title": f"Enchère publique {city}",
+        "price": 70000,
+        "surface": 75,
+        "source": "agorastore"
+    }]
