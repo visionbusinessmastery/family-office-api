d0295610d29671830632b5e99c080545de1d1515
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-07 11:12:39 -0400

    Update routes.py

--- real /routes.py
+++ real /routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from .schemas import RealEstateQuery
 from .service import get_real_estate_intelligence
 
-router = APIRouter(prefix="/real-estate", tags=["Real Estate"])
+router = APIRouter(prefix="/real", tags=["Real"])
 
 @router.post("/")
 def real(query: RealEstateQuery):
