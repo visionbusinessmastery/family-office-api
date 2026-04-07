4938cf818f9f24d4853c2c203d03133d80261b30
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 20:22:26 -0400

    Create yield.py

---
+++ real /analyzers/yield.py
@@ -0,0 +1,11 @@
+import requests
+import os
+
+def calculate_yield(property):
+
+    # estimation simple
+    rent_estimated = property["surface"] * 10  # €/m²
+
+    annual_rent = rent_estimated * 12
+
+    return round((annual_rent / property["price"]) * 100, 2)
