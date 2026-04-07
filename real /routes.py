51bff361dc57da0960111ffec4049aa85b43d991
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 21:32:59 -0400

    Update routes.py

--- real /routes.py
+++ real /routes.py
@@ -5,5 +5,5 @@ from .service import get_real_estate_intelligence
 router = APIRouter()
 
 @router.post("/real")
-def real(query: RealQuery):
+def real(query: Real(Query):
     return get_real(query)
