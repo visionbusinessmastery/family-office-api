6d14c6998685f21b241b70af5621027b0697b97a
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 20:24:22 -0400

    Create imodom.py

---
+++ real /scrappers/imodom.py
@@ -0,0 +1,11 @@
+import requests
+import os
+
+def search_imodom(city: str):
+
+    return [{
+        "title": f"Annonce Imodom {city}",
+        "price": 150000,
+        "surface": 65,
+        "source": "imodom"
+    }]
