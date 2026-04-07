ccf0389429b4790399f242b9eb29bde0931bbb98
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-06 20:33:02 -0400

    Delete real estate directory

--- real estate/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException
-from .schemas import RealEstateQuery
-from .service import get_real_estate_intelligence
-
-router = APIRouter()
-
-@router.post("/real-estate")
-def real_estate_intelligence(query: RealEstateQuery):
-    return get_real_estate_intelligence(query)
