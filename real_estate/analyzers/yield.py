7b5f46df2f5b0bd00352778b86eae6afd9d96dcc
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-10 09:11:06 -0400

    Update yield.py

--- real_estate/analyzers/yield.py
+++ real_estate/analyzers/yield.py
@@ -3,4 +3,4 @@ def calculate_yield(property):
     rent_estimated = property["surface"] * 10
     annual_rent = rent_estimated * 12
 
-return round((annual_rent / property["price"]) * 100, 2)
+    return round((annual_rent / property["price"]) * 100, 2)
