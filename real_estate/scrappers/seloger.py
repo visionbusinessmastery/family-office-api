f0838b5703435995da72a2a8a17284445251f7e6
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-10 12:01:26 -0400

    Create seloger.py

---
+++ real_estate/scrappers/seloger.py
@@ -0,0 +1,10 @@
+def search_seloger(city: str):
+
+    return [
+        {
+            "title": f"Appartement centre {city}",
+            "price": 200000,
+            "surface": 70,
+            "source": "seloger"
+        }
+    ]
