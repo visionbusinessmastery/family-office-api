62bd196935fbc629450458d53db4be1e58e9a7dd
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-13 09:03:43 -0400

    Create bpi.py

---
+++ business/scrappers/bpi.py
@@ -0,0 +1,10 @@
+def search_bpi(sector):
+
+    return [
+        {
+            "title": f"Idée business BPI - {sector}",
+            "description": "Concept rentable avec faible concurrence",
+            "type": "creation",
+            "source": "bpi"
+        }
+    ]
