8597ca6626600c6d2fa2cb47fbec31aaeac16475
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-13 09:05:29 -0400

    Create financement.py

---
+++ business/scrappers/financement.py
@@ -0,0 +1,14 @@
+def get_financements():
+
+    return [
+        {
+            "name": "Prêt d'honneur",
+            "organisme": "Initiative France",
+            "type": "0% loan"
+        },
+        {
+            "name": "Microcrédit",
+            "organisme": "ADIE",
+            "type": "loan"
+        }
+    ]
