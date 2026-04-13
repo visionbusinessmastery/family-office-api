10d16e7ac72b045941dfde6b927c98bf1d934484
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-13 08:49:39 -0400

    Update scoring.py

--- real_estate/analyzers/scoring.py
+++ real_estate/analyzers/scoring.py
@@ -8,9 +8,14 @@ def score_property(property, yield_value, strategy):
         score += 20
 
     price_m2 = property["price"] / property["surface"]
-
+    price_m2_market = estimate_price_m2(query.city)
+    price_m2_property = property["price"] / property["surface"]
+        
     if price_m2 < 2000:
         score += 30
+
+    if price_m2_property < price_m2_market:
+       score += 20
 
     if strategy == "flip" and price_m2 < 1500:
         score += 30
