95eaaa7e532137ebc4b0cee77fae481a2a0c2062
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-13 09:06:06 -0400

    Create transmission.py

---
+++ business/scrappers/transmission.py
@@ -0,0 +1,10 @@
+def search_business_for_sale(sector):
+
+    return [
+        {
+            "title": f"Entreprise à vendre - {sector}",
+            "price": 120000,
+            "location": "France",
+            "source": "fusacq"
+        }
+    ]
