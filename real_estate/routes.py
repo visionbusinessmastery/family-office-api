3f4b737bfdfc88ab13c384b0b2e3802ffe1b9d79
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-13 16:22:12 -0400

    Update routes.py

--- real_estate/routes.py
+++ real_estate/routes.py
@@ -1,14 +1,14 @@
-import logging
 from core.utils import safe_execute
-from fastapi import APIRouter, Depends, HTTPException
-from .schemas import RealEstateQuery
+from fastapi import APIRouter
+from .schemas import RealRequest
 from .service import get_real_estate_intelligence
-from real_estate.schemas import RealRequest
 
 router = APIRouter()
 
 @router.post("/")
 def real(data: RealRequest):
-    try:
-        result = real_estate_service(data)
-        return result
+
+    def _real():
+        return get_real_estate_intelligence(data)
+
+    return safe_execute(_real, module_name="REAL_ESTATE")
