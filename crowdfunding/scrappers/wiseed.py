a0d89eda6d3a8041ba3d4e54f8aa4095f5ef6f94
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-13 12:32:16 -0400

    Add search_wiseed function for crowdfunding data

---
+++ crowdfunding/scrappers/wiseed.py
@@ -0,0 +1,9 @@
+def search_wiseed():
+
+    return [{
+        "name": "Projet immobilier Paris",
+        "expected_return": 9.5,
+        "risk": "medium",
+        "duration": 18,
+        "source": "wiseed"
+    }]
