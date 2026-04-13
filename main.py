a03e295e921ec40dea198cf3442c243146274973
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-13 14:09:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
