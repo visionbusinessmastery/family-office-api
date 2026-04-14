fd1dc46c124ef580cb82236d87392112b6769c4a
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-14 16:42:02 -0400

    Update routes.py

--- crowdfunding/routes.py
+++ crowdfunding/routes.py
@@ -6,5 +6,6 @@ from .service import get_crowdfunding_intelligence
 router = APIRouter()
 
 @router.post("/crowdfunding")
+@limiter.limit("20/minute")
 def crowdfunding(query: CrowdfundingQuery):
     return get_crowdfunding_intelligence(query)
