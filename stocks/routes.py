fc1c72122b81fd7196f94b7937c519e2ceabde87
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-14 16:43:14 -0400

    Update routes.py

--- stocks/routes.py
+++ stocks/routes.py
@@ -1,3 +1,4 @@
+from core.limiter import limiter
 from core.utils import safe_execute
 from fastapi import APIRouter, Depends, HTTPException
 from auth.utils import get_current_user
@@ -7,6 +8,7 @@ from .service import get_stock_data
 router = APIRouter()
 
 @router.post("/stocks/search")
+@limiter.limit("20/minute")
 def search_stocks(request: StockRequest, user: str = Depends(get_current_user)):
 
     data = get_stock_data(request.query)
