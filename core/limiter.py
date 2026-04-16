670898175a148ce5a1bc9013b3f08545ceb1f021
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-16 08:52:03 -0400

    Update limiter.py

--- core/limiter.py
+++ core/limiter.py
@@ -1,4 +1,7 @@
 from slowapi import Limiter
 from auth.utils import get_user_key
 
-limiter = Limiter(key_func=get_user_key)
+limiter = Limiter(
+    key_func=get_user_key,
+    default_limits=["100/minute"]  # 🔥 fallback global
+)
