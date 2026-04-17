1266b78babddf820d80cdd85307134957956dc0b
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-17 11:55:38 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from database import Base
+
 
 class User(Base):
     __tablename__ = "users"
@@ -8,11 +9,12 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
-class Portfolio(Base):
-    __tablename__ = "portfolio"
 
-    id = Column(Integer, primary_key=True)
+class Portfolio(Base):
+    __tablename__ = "portfolios"  # ⚠️ aligné avec tes requêtes SQL
+
+    id = Column(Integer, primary_key=True, index=True)
     asset = Column(String)
     asset_type = Column(String)
-    quantity = Column(Integer)
+    quantity = Column(Float)
     buy_price = Column(Float)
