5804b7974db7540364803bcfdd0ce4854c788e34
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-17 11:27:49 -0400

    Update routes.py

--- crm/routes.py
+++ crm/routes.py
@@ -1,20 +1,34 @@
 from core.limiter import limiter
 from core.utils import safe_execute
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Request
 from .schemas import LeadRequest
 from .odoo import OdooClient
 
 router = APIRouter()
-odoo = OdooClient()
 
+
+# =========================
+# CREATE LEAD
+# =========================
 @router.post("/lead")
 @limiter.limit("10/minute")
 def create_lead(request: Request, data: LeadRequest):
 
     def _create_lead():
-        contact_id = odoo.create_contact(data.name, data.email)
-        return {"contact_id": contact_id}
+
+        user_email = request.state.user_email
+
+        odoo = OdooClient()
+
+        contact_id = odoo.create_contact(
+            name=data.name,
+            email=data.email
+        )
+
+        return {
+            "user": user_email,
+            "contact_id": contact_id
+        }
 
     return safe_execute(_create_lead, module_name="CRM")
-
 
