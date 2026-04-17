905e8eb0919e9c97708017fd41298df548f7af21
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-17 11:34:22 -0400

    Update schemas.py

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel, Field, EmailStr
 from typing import Optional, Dict
 
-# ==================================================
-# MODELS
-# ==================================================
 
+# =========================
+# REGISTER
+# =========================
 class UserRegister(BaseModel):
     email: EmailStr
     password: str = Field(min_length=6, max_length=128)
 
+
+# =========================
+# PROFILE INPUT
+# =========================
 class UserProfileRequest(BaseModel):
     genre: Optional[str] = None
     age: Optional[int] = None
@@ -28,20 +32,3 @@ class UserProfileRequest(BaseModel):
     dettes: Optional[dict] = {}
     epargne: Optional[dict] = {}
     investissements: Optional[dict] = {}
-
-class UserProfile(BaseModel):
-    genre: Optional[str] = None
-    age: Optional[int] = None
-    situation_pro: Optional[str] = None
-    revenus_mensuels: Optional[float] = None
-    revenus_annuels: Optional[float] = None
-    situation_familiale: Optional[str] = None
-    enfants: Optional[bool] = None
-    nb_enfants: Optional[int] = None
-    logement: Optional[str] = None
-    valeur_bien: Optional[float] = None
-    prix_achat: Optional[float] = None
-    dettes: Optional[Dict] = {}
-    epargne: Optional[Dict] = {}
-    investissements: Optional[Dict] = {}
-    
