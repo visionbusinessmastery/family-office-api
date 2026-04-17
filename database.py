041c30bc9c1b48530e020688a21ea0188b88ab68
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-17 11:54:42 -0400

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,19 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL manquant")
+
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -16,6 +25,7 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
