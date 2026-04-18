f0b4287c29b94cb7c29c59149e09eaf9431a9f2a
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-17 22:26:57 -0400

    Update routes.py

--- advisor/routes.py
+++ advisor/routes.py
@@ -3,6 +3,11 @@ from core.utils import safe_execute
 from .schemas import AdvisorRequest
 from .service import advisor_logic
 
+from core.limiter import limiter
+from pydantic import BaseModel
+from .service import get_advisor_premium
+from .service import get_advisor_auto
+
 router = APIRouter()
 
 @router.post("/advisor")
@@ -20,3 +25,37 @@ def advisor(request: Request, data: AdvisorRequest):
         }
 
     return safe_execute(_advisor, module_name="ADVISOR")
+
+@router.post("/advisor/premium")
+@limiter.limit("10/minute")
+def advisor_premium(request: Request, data: AdvisorPremiumRequest):
+
+    def _advisor():
+        user_email = request.state.user_email
+
+        result = get_advisor_premium(user_email, data.message)
+
+        return {
+            "user": user_email,
+            "input": data.message,
+            "result": result
+        }
+
+    return safe_execute(_advisor, module_name="ADVISOR_PREMIUM")
+
+
+@router.post("/advisor/auto")
+@limiter.limit("10/minute")
+def advisor_auto(request: Request):
+
+    def _auto():
+        user_email = request.state.user_email
+
+        result = get_advisor_auto(user_email)
+
+        return {
+            "user": user_email,
+            "result": result
+        }
+
+    return safe_execute(_auto, module_name="ADVISOR_AUTO")
