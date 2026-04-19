8f860dc320eb568c2d25fa75c5200574d57709af
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-18 23:52:58 -0400

    Update seloger_playwright.py

--- real_estate/scrappers/seloger_playwright.py
+++ real_estate/scrappers/seloger_playwright.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import sync_playwright
-from real_estate.config import ZONES, SCRAPER_LIMIT, SUPPORTED_REGIONS
+from real_estate.config import ZONES, SCRAPER_LIMIT
     
 def scrape_seloger(city, max_price):
 
