51c44c87d0c93e3e140a23a1f6f40746ad30877b
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-18 21:39:05 -0400

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,13 @@
+ZONES = {
+    "cyphoma": {
+        "martinique": "martinique",
+        "guadeloupe": "guadeloupe",
+        "guyane": "guyane",
+        "saint-martin": "saint-martin"
+    },
+    "97immo": {
+        "martinique": "martinique",
+        "guadeloupe": "guadeloupe",
+        "guyane": "guyane"
+    }
+}
