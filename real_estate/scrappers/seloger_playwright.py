13926587c86d3e0a17fbf169097a72a76d5996ce
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-18 23:05:01 -0400

    Update seloger_playwright.py

--- real_estate/scrappers/seloger_playwright.py
+++ real_estate/scrappers/seloger_playwright.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import sync_playwright
-from .config import SCRAPER_MATRIX
+from real_estate.config import SCRAPER_MATRIX
 
 def get_sources(zone: str):
     return SCRAPER_MATRIX.get(zone, [])
