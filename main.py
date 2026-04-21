3a8bab268562d84cb23a5546cb78036baa181c0f
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-21 00:29:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ app.state.limiter = limiter
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ton-frontend.com",
-        "http://localhost:3000",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
