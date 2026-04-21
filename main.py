a1f3ad06ced08dc68b90fec4ed62203a0d66024c
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-21 09:02:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,10 +36,7 @@ app.state.limiter = limiter
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://ton-vrai-frontend.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
