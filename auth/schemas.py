3eda73a1cb53cf7466e242e33938b1ef04104753
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-22 08:57:56 -0400

    Update schemas.py

--- auth/schemas.py
+++ auth/schemas.py
@@ -16,22 +16,7 @@ class UserAuth(BaseModel):
 class UserProfileRequest(BaseModel):
     genre: Optional[str] = None
     age: Optional[int] = None
-
     situation_pro: Optional[str] = None
-    revenus_mensuels: Optional[float] = 0
-    revenus_annuels: Optional[float] = 0
-
-    situation_familiale: Optional[str] = None
-    enfants: Optional[bool] = False
-    nb_enfants: Optional[int] = 0
-
-    logement: Optional[str] = None
-    valeur_bien: Optional[float] = 0
-    prix_achat: Optional[float] = 0
-
-    dettes: float = 0
-    epargne: float = 0
-    investissements: float = 0
 
 
 # =========================
