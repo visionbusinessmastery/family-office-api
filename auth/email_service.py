ff6dc278c542d874e937a9bde940de7085cf40b3
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-04-23 19:50:52 -0400

    Update email_service.py

--- auth/email_service.py
+++ auth/email_service.py
@@ -1,19 +1,16 @@
 import os
-import resend
+from resend import Client
 
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 
 if not RESEND_API_KEY:
     raise Exception("RESEND_API_KEY manquante")
 
-resend.api_key = RESEND_API_KEY
+client = Client(RESEND_API_KEY)
 
 FRONT_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 
-# =========================
-# SEND EMAIL VERIFICATION
-# =========================
 def send_verification_email(to_email: str, token: str):
 
     verify_link = f"{FRONT_URL}/verify-email?token={token}"
@@ -21,7 +18,7 @@ def send_verification_email(to_email: str, token: str):
     print("🔗 VERIFY LINK:", verify_link)
 
     try:
-        response = resend.Emails.send({
+        response = client.emails.send({
             "from": "Vision Business Mastery <onboarding@resend.dev>",
             "to": to_email,
             "subject": "Active ton compte Vision Business Mastery",
@@ -44,8 +41,6 @@ def send_verification_email(to_email: str, token: str):
                         </a>
 
                         <p style="margin-top:20px;font-size:12px;">
-                            Si le bouton ne fonctionne pas :
-                            <br/>
                             {verify_link}
                         </p>
                     </body>
