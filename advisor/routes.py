3434cb4608f6902a08cc874198717b6c85dcc72c
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-01 22:47:05 -0400

    Update routes.py

--- advisor/routes.py
+++ advisor/routes.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter, Request
 from core.utils import safe_execute
 from core.limiter import limiter
 
-from .schemas import AdvisorRequest, AdvisorPremiumRequest
-from .service import advisor_logic
+from .schemas import (
+    AdvisorRequest,
+    AdvisorPremiumRequest,
+    AdvisorEliteRequest
+)
+
 from .service import (
     get_advisor_free,
     get_advisor_premium,
@@ -12,6 +16,10 @@ from .service import (
 
 router = APIRouter()
 
+
+# =========================
+# FREE
+# =========================
 @router.post("/advisor")
 @limiter.limit("10/minute")
 def advisor(request: Request, data: AdvisorRequest):
@@ -27,8 +35,12 @@ def advisor(request: Request, data: AdvisorRequest):
             "result": result
         }
 
-    return safe_execute(_advisor, module_name="ADVISOR")
+    return safe_execute(_advisor, module_name="ADVISOR_FREE")
 
+
+# =========================
+# PREMIUM
+# =========================
 @router.post("/advisor/premium")
 @limiter.limit("10/minute")
 def advisor_premium(request: Request, data: AdvisorPremiumRequest):
@@ -47,18 +59,22 @@ def advisor_premium(request: Request, data: AdvisorPremiumRequest):
     return safe_execute(_advisor, module_name="ADVISOR_PREMIUM")
 
 
+# =========================
+# ELITE
+# =========================
 @router.post("/advisor/elite")
 @limiter.limit("10/minute")
-def advisor_elite(request: Request):
+def advisor_elite(request: Request, data: AdvisorEliteRequest):
 
-    def _auto():
+    def _advisor():
         user_email = request.state.user_email
 
-        result = get_advisor_elite(user_email)
+        result = get_advisor_elite(user_email, data.message)
 
         return {
             "user": user_email,
+            "input": data.message,
             "result": result
         }
 
-    return safe_execute(_auto, module_name="ADVISOR_AUTO")
+    return safe_execute(_advisor, module_name="ADVISOR_ELITE")
