4eaf39bfd2b862e7c921101aeba7b67e93e4bcad
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-01 20:43:43 -0400

    Create market.py

---
+++ data_engine/market.py
@@ -0,0 +1,9 @@
+from market.service import get_market_news, get_google_news
+
+
+def get_news(query: str):
+
+    news = get_market_news(query)
+    news += get_google_news(query)
+
+    return news[:10]
