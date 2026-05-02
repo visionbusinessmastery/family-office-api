de0a11e72626b183836205c8240c0f5b9af0b9e6
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-01 22:02:26 -0400

    Update schemas.py

--- advisor/schemas.py
+++ advisor/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 class AdvisorRequest(BaseModel):
-    message: str  # ex: "j’ai 10k que faire"
+    message: str
 
 class AdvisorPremiumRequest(BaseModel):
     message: str
