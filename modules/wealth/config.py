3aae5faabbe2ea58990336626680f78157e76f63
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 12:11:54 -0400

    Add configuration for wealth module

---
+++ modules/wealth/config.py
@@ -0,0 +1,12 @@
+# =========================
+# CONFIG WEALTH
+# =========================
+MODULE_NAME = "wealth"
+
+DESCRIPTION = """
+Construction de richesse,
+allocation patrimoniale,
+liberté financière et stratégie long terme.
+"""
+
+ENABLED = True
