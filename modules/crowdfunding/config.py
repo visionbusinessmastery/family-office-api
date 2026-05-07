a1cb401b77d0d0ab33889f0fe763fc52f20176f7
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 11:42:35 -0400

    Add crowdfunding configuration file

---
+++ modules/crowdfunding/config.py
@@ -0,0 +1,11 @@
+# =========================
+# CONFIG CROWDFUNDING
+# =========================
+MODULE_NAME = "crowdfunding"
+
+DESCRIPTION = """
+Crowdfunding, investissement participatif,
+private equity accessible et rendement alternatif.
+"""
+
+ENABLED = True
