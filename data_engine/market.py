6320108d130c956aa2de51ee4790468389bf9387
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 08:57:31 -0400

    Delete data_engine directory

--- data_engine/market.py
+++
@@ -1,9 +0,0 @@
-from data_engine.news_service import get_market_news, get_google_news
-
-
-def get_news(query: str):
-
-    news = get_market_news(query)
-    news += get_google_news(query)
-
-    return news[:10]
