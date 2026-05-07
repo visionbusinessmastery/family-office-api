2db8e75d4fb53ba5d4bf1a66785aa45718d38c6b
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 11:42:07 -0400

    Add configuration for franchise module

---
+++ modules/franchise/config.py
@@ -0,0 +1,11 @@
+# =========================
+# CONFIG FRANCHISE
+# =========================
+MODULE_NAME = "franchise"
+
+DESCRIPTION = """
+Franchise business,
+licensing et revenus semi-passifs.
+"""
+
+ENABLED = True
