4426c7811ceffa100606897972848a8f0744a0f1
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 16:06:27 -0400

    Create config.py

---
+++ modules/market/config.py
@@ -0,0 +1,12 @@
+# =========================
+# CONFIG MARKET
+# =========================
+MODULE_NAME = "market"
+
+DESCRIPTION = """
+Analyse macroéconomique globale,
+tendances financières,
+sentiment de marché et opportunités globales.
+"""
+
+ENABLED = True
