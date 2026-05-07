d7380e9754a93f10885d8ed0488a20ff7e32e6af
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 12:12:17 -0400

    Add configuration for commodities module

---
+++ modules/commodities/config.py
@@ -0,0 +1,11 @@
+# =========================
+# CONFIG COMMODITIES
+# =========================
+MODULE_NAME = "commodities"
+
+DESCRIPTION = """
+Matières premières,
+métaux précieux et diversification stratégique.
+"""
+
+ENABLED = True
