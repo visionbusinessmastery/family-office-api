c34b3b9055754c7e8f836b455cd773bed5387467
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 11:41:31 -0400

    Add trading configuration module

---
+++ modules/trading/config.py
@@ -0,0 +1,11 @@
+# =========================
+# CONFIG TRADING
+# =========================
+MODULE_NAME = "trading"
+
+DESCRIPTION = """
+Trading, marchés financiers,
+ETF, swing trading et stratégies avancées.
+"""
+
+ENABLED = True
