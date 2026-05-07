f735be4708268455ca23427badfed2a75ee61dc1
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 11:40:09 -0400

    Create config.py for startup module
    Add configuration settings for startup module.

---
+++ modules/startup/config.py
@@ -0,0 +1,11 @@
+# =========================
+# CONFIG STARTUP
+# =========================
+MODULE_NAME = "startup"
+
+DESCRIPTION = """
+Création et croissance startup,
+MVP et levées de fonds.
+"""
+
+ENABLED = True
