002da9b7e85e29bc10b57d3b169c90889fa12829
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 16:06:01 -0400

    Create config.py

---
+++ modules/stocks/config.py
@@ -0,0 +1,12 @@
+# =========================
+# CONFIG STOCKS
+# =========================
+MODULE_NAME = "stocks"
+
+DESCRIPTION = """
+Analyse actions, ETF,
+portefeuille boursier,
+opportunités equity et trading long terme.
+"""
+
+ENABLED = True
