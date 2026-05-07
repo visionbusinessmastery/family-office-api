8c229193a6a69efc37ddc9d55f3f99203fe153f1
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-05-07 11:39:33 -0400

    Add configuration for private equity module

---
+++ modules/private_equity/config.py
@@ -0,0 +1,11 @@
+# =========================
+# CONFIG PRIVATE EQUITY
+# =========================
+MODULE_NAME = "private_equity"
+
+DESCRIPTION = """
+Acquisition d'entreprises,
+holding et stratégies patrimoniales.
+"""
+
+ENABLED = True
