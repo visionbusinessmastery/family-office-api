f83045abd05767bbd00daf09c79ebab596762aaa
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-06-08 15:21:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://family-office-api-n4sv.onrender.com",
+        "https://https://family-office-api-1.onrender.com",
         "https://vision-business.com",
         "https://family-office-front.vercel.app",
     ],
